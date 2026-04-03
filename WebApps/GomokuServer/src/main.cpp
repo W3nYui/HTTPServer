1f9208c28497546e38d2a00d52d5127e2d51b21d
W3nYui <1421910697@qq.com>
2026-04-03 20:27:53 +0800

    开始学习顶层

--- WebApps/GomokuServer/src/main.cpp
+++ WebApps/GomokuServer/src/main.cpp
@@ -8,21 +8,22 @@
 
 int main(int argc, char* argv[])
 {
+  // 利用muduo的日志输出当前pid
   LOG_INFO << "pid = " << getpid();
-  
+
   std::string serverName = "HttpServer";
   int port = 80;
   
   // 参数解析
   int opt;
   const char* str = "p:";
-  while ((opt = getopt(argc, argv, str)) != -1)
+  while ((opt = getopt(argc, argv, str)) != -1) // getopt函数会返回当前解析到的选项字符，如果没有更多选项可供解析，则返回-1
   {
     switch (opt)
     {
       case 'p':
       {
-        port = atoi(optarg);
+        port = atoi(optarg); // 如果命令行调用参数为 -p + 端口号，则optarg会指向该端口号字符串，atoi函数将其转换为整数并赋值给port变量
         break;
       }
       default:
@@ -30,8 +31,8 @@ int main(int argc, char* argv[])
     }
   }
   
-  muduo::Logger::setLogLevel(muduo::Logger::WARN);
-  GomokuServer server(port, serverName);
-  server.setThreadNum(4);
+  muduo::Logger::setLogLevel(muduo::Logger::WARN); // 设定日志级别为WARN，减少日志输出量
+  GomokuServer server(port, serverName); // 创建HTTP服务器实例 设定端口号与服务器名称
+  server.setThreadNum(4); // 设置服务器线程数为4，允许服务器同时处理多个请求，提高性能
   server.start();
 }
